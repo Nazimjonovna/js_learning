ca6e0c641dd6abb42bb1b89c97a331e7a00cb023
Nazimjonovna <sturayeva000@gmail.com>
2024-10-09 19:40:13 +0500

    4ta dars qoldi

---
+++ work_html.js
@@ -0,0 +1,6 @@
+const username = document.getElementById('username');
+const neww = createElement('h1');
+const text = createTextNode(`${username} Yaxshimisiz)`);
+neww.appendChild(text)
+const d = document.getElementById('h');
+d.appendChild(neww);
